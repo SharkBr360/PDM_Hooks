de8a2e9860b6226c931a128aa37d2631f953aeba
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-10-21 18:24:50 -0300

    Apostila 08 - React Hooks: secao 2.6

---
+++ src/components/Accordion.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card } from 'primereact/card'
+import "./Accordion.css"
+
+const Accordion = ({ itens }) => {
+    const expressaoJSX = itens.map((item, indice) => {
+        return (
+            <Card key={indice} className="border-1 border-400">
+                <div className="div">
+                    <i className='pi pi-angle-down'></i>
+                    <h5 className='inline ml-3'>{item.titulo}</h5>
+                </div>
+                <p>{item.conteudo}</p>
+            </Card>
+        )
+    })
+
+return (
+        <div>
+            {expressaoJSX}
+        </div>
+    ) 
+}
+      
+export default Accordion
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
-import { Card } from 'primereact/card'
+import Accordion from './Accordion'
+
+const itens = [
+  {
+      titulo: "Java",
+      conteudo:"Linguagem compilada e interpretada"
+  },
+  {
+      titulo: "Python",
+      conteudo:"Linguagem interpretada e dinamincamente tipada"
+  },
+  {
+      titulo: "Javascript",
+      conteudo:"Interpretado. Executa do lado do cliente e do lado do servidor também"
+  }
+]
+
 const App = () => {
   return (
-    <Card>App</Card>
+    <div>
+      <Accordion itens={itens} />
+    </div>
   )
 }
 
